5fc2bba8a117dafb7779bef4dde239af0ad3f709
suuu <holic1208@daum.net>
2021-11-16 15:39:17 +0900

    step3

--- 10_var.tf
+++ 10_var.tf
@@ -1,188 +1,188 @@
 variable "true" {
-  type    = bool
-  default = true
+  type = bool
+  #  default = true
 }
 
 variable "name" {
-  type    = string
-  default = "suuu"
+  type = string
+  #  default = "suuu"
 }
 #id 쪽에는 ${var.name} 사용x!!
 
 variable "region" {
-  type    = string
-  default = "ap-northeast-2"
+  type = string
+  #  default = "ap-northeast-2"
 }
 
 variable "region_instance" {
-  type    = string
-  default = "ap-northeast-2a"
+  type = string
+  #  default = "ap-northeast-2a"
 }
 
 variable "ava" {
-  type    = list(string)
-  default = ["a", "c"]
+  type = list(string)
+  #  default = ["a", "c"]
 }
 
 variable "key" {
-  type    = string
-  default = "suuu-key"
+  type = string
+  #  default = "suuu-key"
 }
 
 variable "cidr_main" {
-  type    = string
-  default = "10.0.0.0/16"
+  type = string
+  #  default = "10.0.0.0/16"
 }
 
 variable "cidr_public" {
-  type    = list(string)
-  default = ["10.0.0.0/24", "10.0.2.0/24"]
+  type = list(string)
+  #  default = ["10.0.0.0/24", "10.0.2.0/24"]
 }
 
 variable "cidr_private" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.3.0/24"]
+  type = list(string)
+  #  default = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
 variable "cidr_privatedb" {
-  type    = list(string)
-  default = ["10.0.4.0/24", "10.0.5.0/24"]
+  type = list(string)
+  #  default = ["10.0.4.0/24", "10.0.5.0/24"]
 }
 
 variable "cidr_route" {
-  type    = string
-  default = "0.0.0.0/0"
+  type = string
+  #  default = "0.0.0.0/0"
 }
 
 variable "cidr_ipv6" {
-  type    = string
-  default = "::/0"
+  type = string
+  #  default = "::/0"
 }
 
 variable "sg_ICMP" {
-  type    = string
-  default = "Allow ICMP"
+  type = string
+  #  default = "Allow ICMP"
 }
 variable "sg_HTTP" {
-  type    = string
-  default = "Allow HTTP"
+  type = string
+  #  default = "Allow HTTP"
 }
 
 variable "sg_SSH" {
-  type    = string
-  default = "Allow SSH"
+  type = string
+  #  default = "Allow SSH"
 }
 
 variable "sg_SQL" {
-  type    = string
-  default = "Allow SQL"
+  type = string
+  #  default = "Allow SQL"
 }
 
 variable "SSH_port" {
-  type    = number
-  default = 22
+  type = number
+  #  default = 22
 }
 
 variable "HTTP_port" {
-  type    = number
-  default = 80
+  type = number
+  #  default = 80
 }
 
 variable "SQL_port" {
-  type    = number
-  default = 3306
+  type = number
+  #  default = 3306
 }
 
 variable "ALL_port" {
-  type    = number
-  default = 0
+  type = number
+  #  default = 0
 }
 
 variable "tcp_protocol" {
-  type    = string
-  default = "tcp"
+  type = string
+  #  default = "tcp"
 }
 
 variable "icmp_protocol" {
-  type    = string
-  default = "icmp"
+  type = string
+  #  default = "icmp"
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t2.micro"
+  type = string
+  #  default = "t2.micro"
 }
 
 variable "ec2_private_ip" {
-  type    = string
-  default = "10.0.0.11"
+  type = string
+  #  default = "10.0.0.11"
 }
 
 variable "protocol_HTTP" {
-  type    = string
-  default = "HTTP"
+  type = string
+  #  default = "HTTP"
 }
 
 variable "health_path" {
-  type    = string
-  default = "/index.html"
+  type = string
+  #  default = "/health.html"
 }
 
 variable "health_port" {
-  type    = string
-  default = "traffic-port"
+  type = string
+  #  default = "traffic-port"
 }
 
 variable "health_type" {
-  type    = string
-  default = "EC2"
+  type = string
+  #  default = "EC2"
 }
 
 variable "listener_port" {
-  type    = string
-  default = "80"
+  type = string
+  #  default = "80"
 }
 
 variable "RDS_allocate_sto" {
-  type    = number
-  default = 20
+  type = number
+  #  default = 20
 }
 
 variable "RDS_ins_type" {
-  type    = string
-  default = "db.t2.micro"
+  type = string
+  #  default = "db.t2.micro"
 }
 
 variable "RDS_eng_type" {
-  type    = string
-  default = "mysql"
+  type = string
+  #  default = "mysql"
 }
 
 variable "RDS_sto_type" {
-  type    = string
-  default = "gp2"
+  type = string
+  #  default = "gp2"
 }
 
 variable "RDS_version" {
-  type    = string
-  default = "8.0"
+  type = string
+  #  default = "8.0"
 }
 variable "RDS_dbname" {
-  type    = string
-  default = "test"
+  type = string
+  #  default = "test"
 }
 
 variable "RDS_user" {
-  type    = string
-  default = "admin"
+  type = string
+  #  default = "admin"
 }
 
 variable "RDS_password" {
-  type    = string
-  default = "It12345!"
+  type = string
+  #  default = "It12345!"
 }
 
 variable "RDS_parag_name" {
-  type    = string
-  default = "default.mysql8.0"
+  type = string
+  #  default = "default.mysql8.0"
 }
